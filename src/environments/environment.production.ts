225d9a960a472c457c18e3fba1d5ad631370f300
Fernando32117 <nando32117@gmail.com>
2026-03-17 21:49:22 -0300

    feat: envs

--- src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  googleBooksApiKey: 'AIzaSyDWlvXEjlkiDRRkKDgqXIXD8NSEJ4xAIGg',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  googleBooksApiKey: 'AIzaSyBALRl3ucTWNkhMRDeua-KP45D0v334R7g',
-};
